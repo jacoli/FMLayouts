ed9b3e9827d8fd8e7477137fc4a1d86a658f9834
jacoli <jaco.lcg@gmail.com>
2017-08-10 10:59:03 +0800

    update

--- FMLayouts.podspec
+++ FMLayouts.podspec
@@ -2,10 +2,10 @@ Pod::Spec.new do |s|
   s.name         = "FMLayouts"
   s.version      = "0.0.1"
   s.summary      = "A easy way to layout views in iOS using linear, flow etc., similar to layout mechanism in android."
-  s.homepage     = "https://github.com/fanmeiwireless/FMLayouts"
+  s.homepage     = "https://github.com/jacoli/FMLayouts"
   s.license      = "MIT"
   s.authors      = { "jacoli" => "jaco.lcg@gmail.com" }
-  s.source       = { :git => "https://github.com/fanmeiwireless/FMLayouts.git", :tag => "0.0.1" }
+  s.source       = { :git => "https://github.com/jacoli/FMLayouts.git", :tag => "1.0.0" }
   s.frameworks   = 'Foundation', 'UIKit'
   s.platform     = :ios, '7.0'
   s.source_files = 'FMLayouts/Sources/*.{h,m}'
--- FMLayouts/Sources/FMFlowView.m
+++ FMLayouts/Sources/FMFlowView.m
@@ -7,68 +7,192 @@
 //
 
 #import "FMFlowView.h"
+#import "UIView+FMLayoutsUtils.h"
 
 @interface FMFlowView ()
-@property (nonatomic, strong) NSMutableArray<__kindof UIView *> *views;
+@property (nonatomic, strong) NSMutableArray<__kindof UIView *> *flowSubviews;
+@property (nonatomic, strong) NSMutableArray<__kindof UIView *> *flagViews;//记录最下面columns个视图
+
+@property (nonatomic, strong) UIView *frontView;//当flowViewType为FMFlowViewSubviewsEqualWidthType时，标记上一个底部最低的视图
+@property (nonatomic, assign) FMFlowViewType flowViewType;
+
+@property (nonatomic, assign) NSInteger columns;
 @end
 
 @implementation FMFlowView
 
-- (NSMutableArray<UIView *> *)views {
-    if (nil == _views) {
-        _views = [[NSMutableArray alloc]init];
+- (instancetype)initSubviewsEquelWidthTypeWithFrame:(CGRect)frame column:(NSInteger)columns {
+    self = [super initWithFrame:frame];
+    self.columns = MAX(columns, 1);
+    self.flowViewType = FMFlowViewSubviewsEqualWidthType;
+    return self;
+}
+
+- (instancetype)initSubviewsEqualHeightTypeWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    self.flowViewType = FMFlowViewSubviewsEqualHeightType;
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    return [self initSubviewsEqualHeightTypeWithFrame:frame];
+}
+
+- (instancetype)init {
+    return [self initSubviewsEqualHeightTypeWithFrame:CGRectZero];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    return [self initSubviewsEqualHeightTypeWithFrame:CGRectZero];
+}
+
+- (NSMutableArray<UIView *> *)flowSubviews {
+    if (nil == _flowSubviews) {
+        _flowSubviews = [[NSMutableArray alloc]init];
     }
-    return _views;
+    return _flowSubviews;
+}
+
+- (NSMutableArray<UIView *> *)flagViews {
+    if (nil == _flagViews) {
+        _flagViews = [[NSMutableArray alloc]init];
+    }
+    return _flagViews;
+}
+
+- (NSArray<UIView *> *)views {
+    return self.flowSubviews;
 }
 
 - (void)addViews:(NSArray *)views {
-    if (views.count) {
-        for (UIView *view in views) {
-            [self addView:view];
-        }
+    if (views.count > 0) {
+        __weak __typeof(self) weakSelf = self;
+        [views enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            [weakSelf addView:obj];
+        }];
     }
+    else {
+        [self resetContentSize];
+    }
+}
+
+- (void)removeAllViews {
+    [self.flowSubviews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [obj removeFromSuperview];
+    }];
+    [self.flowSubviews removeAllObjects];
+    [self reloadView];
 }
 
 - (void)addView:(UIView *)view {
     if (view) {
         [self resetViewFrame:view];
         [self addSubview:view];
-        [self.views addObject:view];
+        [self.flowSubviews addObject:view];
         [self resetContentSize];
     }
-    
+}
+
+- (void)removeView:(UIView *)view {
+    if (view) {
+        if ([self.flowSubviews containsObject:view]) {
+            [view removeFromSuperview];
+            [self.flowSubviews removeObject:view];
+            [self reloadView];
+        }
+    }
+}
+
+- (void)reloadView {
+    NSArray *views = [NSArray arrayWithArray:self.flowSubviews];
+    [self.flowSubviews removeAllObjects];
+    [self addViews:views];
 }
 
 - (void)resetViewFrame:(UIView *)view {
     if (view) {
-        if (self.views.count > 0) {
-            if (CGRectGetWidth(view.frame) > CGRectGetWidth(self.frame) - (CGRectGetMaxX(self.views.lastObject.frame) + self.layoutInset.right + self.layoutSpace)) {
-                view.frame = CGRectMake(self.layoutInset.left, CGRectGetMaxY(self.views.lastObject.frame) + self.layoutSpace, CGRectGetWidth(view.frame), CGRectGetHeight(view.frame));
-            }
-            else {
-                view.frame = CGRectMake(CGRectGetMaxX(self.views.lastObject.frame) + self.layoutSpace,CGRectGetMinY(self.views.lastObject.frame),CGRectGetWidth(view.frame), CGRectGetHeight(view.frame));
-            }
+        if (self.flowViewType == FMFlowViewSubviewsEqualHeightType) {
+            [self setEqualHeightTypeViewFrame:view];
         }
-        else {
-            view.frame = CGRectMake(self.layoutInset.left, self.layoutInset.top, CGRectGetWidth(view.frame), CGRectGetHeight(view.frame));
-        }
-        if (CGRectGetWidth(view.frame) > (CGRectGetWidth(self.frame) - self.layoutInset.left - self.layoutInset.right)) {
-            CGRect rect = view.frame;
-            rect.size.width = CGRectGetWidth(self.frame) - self.layoutInset.left - self.layoutInset.right;
-            view.frame = rect;
+        else if (self.flowViewType == FMFlowViewSubviewsEqualWidthType) {
+            [self setEqualWidthTypeViewFrame:view];
         }
     }
 }
 
-- (void)resetContentSize {
-    if (self.views.count) {
-        CGFloat bottomY = CGRectGetMaxY(self.views.lastObject.frame) + self.layoutInset.bottom;
-        if (bottomY > CGRectGetHeight(self.frame)) {
-            self.contentSize = CGSizeMake(CGRectGetWidth(self.frame), bottomY);
+- (void)setEqualWidthTypeViewFrame:(UIView *)view {
+    CGRect rect = view.frame;
+    CGFloat viewWidth = (self.fm_width - (self.layoutInset.left + self.layoutInset.right) - (self.columns - 1) * self.layoutSpace) / self.columns;
+    CGFloat viewHeight = view.fm_width > 0 ? viewWidth * (view.fm_height / view.fm_width) : 0;
+    rect.size.width = viewWidth;
+    rect.size.height = viewHeight;
+    
+    if (self.flowSubviews.count == 0) {
+        rect.origin.x = self.layoutInset.left;
+        rect.origin.y = self.layoutInset.top;
+    }
+    else if (self.flowSubviews.count < self.columns) {
+        rect.origin.x = self.flowSubviews.lastObject.fm_right + self.layoutSpace;
+        rect.origin.y = self.flowSubviews.lastObject.fm_top;
+    }
+    else {
+        UIView *uppermorstView = [self getUppermostView];
+        if (uppermorstView) {
+            rect.origin.x = uppermorstView.fm_left;
+            rect.origin.y = uppermorstView.fm_bottom + self.layoutSpace;
+            [self.flagViews removeObject:uppermorstView];
+        }
+    }
+    [self.flagViews addObject:view];
+    view.frame = rect;
+}
+
+//最上面的视图
+- (UIView *)getUppermostView {
+    if (self.columns == 1) {
+        return self.flowSubviews.lastObject;
+    }
+    UIView __block *view = nil;
+    [self.flagViews enumerateObjectsUsingBlock:^(__kindof UIView * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (view == nil) {
+            view = obj;
+        }
+        else if (view.fm_bottom > obj.fm_bottom){
+            view = obj;
+        }
+    }];
+    return view;
+}
+
+- (void)setEqualHeightTypeViewFrame:(UIView *)view {
+    CGRect rect = view.frame;
+    if (self.flowSubviews.count > 0) {
+        if (view.fm_width > (self.fm_width - (self.flowSubviews.lastObject.fm_right + self.layoutInset.right + self.layoutSpace))) {
+            rect.origin.x = self.layoutInset.left;
+            rect.origin.y = self.flowSubviews.lastObject.fm_bottom + self.layoutSpace;
         }
         else {
-            self.contentSize = self.frame.size;
+            rect.origin.x = self.flowSubviews.lastObject.fm_right + self.layoutSpace;
+            rect.origin.y = self.flowSubviews.lastObject.fm_top;
         }
+    }
+    else {
+        rect.origin.x = self.layoutInset.left;
+        rect.origin.y = self.layoutInset.top;
+    }
+    if (view.fm_width > (self.fm_width - self.layoutInset.left - self.layoutInset.right)) {
+        rect.size.width = self.fm_width - self.layoutInset.left - self.layoutInset.right;
+    }
+    view.frame = rect;
+}
+
+- (void)resetContentSize {
+    if (self.flowSubviews.count > 0) {
+        CGFloat bottomY = self.flowSubviews.lastObject.fm_bottom + self.layoutInset.bottom;
+        self.contentSize = CGSizeMake(self.fm_width, bottomY);
+    }
+    else {
+        self.contentSize = CGSizeZero;
     }
 }
 
